3b7b4661ffe561158885d1ca73c454949d66c9da
Egemen Yiğit Kömürcü <ekomurcu@sabanciuniv.edu>
2019-11-27 22:16:28 +0100

    Update all

--- src/black-box/testSort.py
+++ src/black-box/testSort.py
@@ -3,10 +3,13 @@ import unittest
 class TestSort(unittest.TestCase):
     def setUp(self):
         self.sortType=['quicksort', 'mergesort', 'heapsort', 'stable']
-        self.intArr=np.array([[5,-2],[8,-1],[1,8],[0,-4]])
-        self.resIntCol=np.array([[0,-4],[1,-2],[5, -1],[8,8]])
-        self.resIntRow=np.array([[-2,5],[-1,8],[1, 8],[-4,0]])
-        self.resIntFlatten=np.array([-4,-2,-1,0,1,5,8,8])
+        self.int1DArr=np.array([-2,2.6, NaN, -8.9,0])
+        self.int2DArr=np.array([[5,-2],[8,-1],[1,8],[0,-4]])
+        self.resInt2DCol=np.array([[0,-4],[1,-2],[5, -1],[8,8]])
+        self.resInt2DRow=np.array([[-2,5],[-1,8],[1, 8],[-4,0]])
+        self.resInt2DFlatten=np.array([-4,-2,-1,0,1,5,8,8])
+    def testInt1Dsort(self):
+        for srt in self.sortType:
 
     def testInt2Dsort(self):
         for srt in self.sortType:
