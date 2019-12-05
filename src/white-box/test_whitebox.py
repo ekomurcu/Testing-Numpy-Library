b8f5c438e32f3c168c25bec1e3f3d50834ef4648
Egemen Yiğit Kömürcü <ekomurcu@sabanciuniv.edu>
2019-12-05 01:45:34 +0100

    Remove print statements

--- src/white-box/test_whitebox.py
+++ src/white-box/test_whitebox.py
@@ -9,15 +9,15 @@ import numpy as np
 import whitebox as w
 #polyval(input,constants)
 Alist= list([1,2,3])
-print(w.polyval(np.nan,np.nan))#%69
-print(w.polyval(2,np.nan))#%69
-print(w.polyval(Alist,np.nan))#%77
+w.polyval(np.nan,np.nan)#%69
+w.polyval(2,np.nan)#%69
+w.polyval(Alist,np.nan)#%77
 
-print(w.polyval(5,5))#%85
-print(w.polyval(5,Alist))#%92
+w.polyval(5,5)#%85
+w.polyval(5,Alist)#%92
 
-print(w.polyval(list([5,4]),Alist))#%92
-print(w.polyval(np.array([[5,5], [5,5]]),Alist))#%100
+w.polyval(list([5,4]),Alist)#%92
+w.polyval(np.array([[5,5], [5,5]]),Alist)#%100
 
 cov.stop()
 cov.save()
