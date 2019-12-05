dc9da7ff573bb638a48e97f61cb3aad25561af15
Egemen Yiğit Kömürcü <ekomurcu@sabanciuniv.edu>
2019-12-05 02:35:35 +0100

    Update all

--- src/white-box/test_whitebox.py
+++ src/white-box/test_whitebox.py
@@ -14,7 +14,7 @@ w.polyval(2,np.nan)#%69
 w.polyval(Alist,np.nan)#%77
 
 w.polyval(5,5)#%85
-w.polyval(5,Alist)#%92
+print(w.polyval(2,Alist))#%92  
 
 w.polyval(list([5,4]),Alist)#%92
 w.polyval(np.array([[5,5], [5,5]]),Alist)#%100
--- src/white-box/testpolyval.py
+++ src/white-box/testpolyval.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+print('(3x^2 + 2x + 1)(2)= ' + str(np.polyval(2,[1, 2, 3])))
+#prints (3x^2 + 2x + 1)(2)= 17
 
 A23 = np.array([[11,12,13],[21,22,23]],np.int32)
 
