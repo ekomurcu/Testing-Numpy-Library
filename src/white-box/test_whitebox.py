270719f8c4da2d47a8a81a97de86e2839ca3050d
Egemen Yiğit Kömürcü <ekomurcu@sabanciuniv.edu>
2019-12-05 01:34:16 +0100

    Update whitebox

--- src/white-box/test_whitebox.py
+++ src/white-box/test_whitebox.py
@@ -9,8 +9,6 @@ import numpy as np
 import whitebox as w
 #polyval(input,constants)
 Alist= list([1,2,3])
-
-
 print(w.polyval(np.nan,np.nan))#%69
 print(w.polyval(2,np.nan))#%69
 print(w.polyval(Alist,np.nan))#%77
