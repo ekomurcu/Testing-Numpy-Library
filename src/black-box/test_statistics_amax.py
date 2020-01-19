d1c3e0407b7f7970236506da07e4483c09d4ab6f
Anton Gildebrand <anton@kodfabriken.nu>
2020-01-19 23:19:54 +0100

    Commented amax

--- src/black-box/test_statistics_amax.py
+++ src/black-box/test_statistics_amax.py
@@ -3,39 +3,47 @@ import numpy as np
 import math
 
 
+# Test suite for the amax function of the statistics module
 class TestAMax(unittest.TestCase):
+    # Test that amax throws an exception if we try to get the minimum value of empty input
     def test_empty_input(self):
         with self.assertRaises(ValueError) as context:
-            np.amin([])
+            np.amax([])
 
         self.assertTrue('zero-size array' in str(context.exception))
 
+    # Test that amax can handle infinity inputs
     def test_infinity_input(self):
         self.assertEqual(np.amax([
             math.inf,
             -math.inf
         ]), math.inf)
 
+    # Test that amax can handle simple one dimension positive values
     def test_one_dimension_positive(self):
         self.assertEqual(np.amax(range(1000, 0, -1)), 1000)
         self.assertEqual(np.amax(range(20, 10, -1)), 20)
 
+    # Test that amax can handle simple one dimension negative values
     def test_one_dimension_negative(self):
         self.assertEqual(np.amax(range(-1000, 0)), -1)
         self.assertEqual(np.amax(range(-1000, 1)), 0)
 
+    # Test that amax can handle two dimensional positive values
     def test_two_dimension_positive(self):
         self.assertEqual(np.amax([
             range(10, 0, -1),
             range(20, 10, -1)
         ]), 20)
 
+    # Test that amax can handle two dimensional negative values
     def test_two_dimension_negative(self):
         self.assertEqual(np.amax([
             range(0, -10, -1),
             range(-10, -20, -1)
         ]), 0)
 
+    # Test that amax can handle imaginary input values
     def test_one_dimension_imaginary(self):
         self.assertEqual(np.amax([
             1j,
@@ -43,6 +51,7 @@ class TestAMax(unittest.TestCase):
             3j
         ]), 3j)
 
+    # Test that amax can handle complex input values
     def test_one_dimension_complex(self):
         self.assertEqual(np.amax([
             1j + 4,
@@ -50,6 +59,7 @@ class TestAMax(unittest.TestCase):
             3j + 1
         ]), 1j + 4)
 
+    # Test that amax can handle a mix of complex and non complex values
     def test_one_dimension_complex_non_complex_mix(self):
         self.assertEqual(np.amax([
             1j + 4,
